ca8bf7ec4d3f9b48e47d727a97a08368ddcdb18f
sumangourab <sumangourab77@gmail.com>
2022-11-10 22:55:33 +0530

    push 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import './Heading';
+import Heading from "./Heading";
+import './InputQuery';
+import InputQuery from "./InputQuery";
+import './SubHeading';
+import SubHeading from "./SubHeading";
+import './SubmitButton'
+import SubmitButton from "./SubmitButton";
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <InputQuery />
+      <SubHeading />
+      <SubmitButton />
     </div>
   )
 }
